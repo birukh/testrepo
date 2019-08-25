7f62b966fb396bbc8a702735c2d08eb76358f6cb
birukh <birukae@gmail.com>
2019-08-25 16:29:59 -0700

    updates

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
 print("Hello wrold")
 print("reply")
-print("origio-master")
-
-print("Hello from branch 02")
-print("Hello line 2 from branch 02")
-print("branchA and branch 03")
-
-print("addition of new file")
-print("added new branch: branch05")
-print("modify for branch 06")
 
